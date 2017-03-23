fcecf1d7b6e43259bebb53b5013c18b2568840dd
Sabieh Ahmed <sabieh.ahmed@gmail.com>
2017-03-23 03:44:53 +0000

    Apply fixes from StyleCI

--- src/Models/Device.php
+++ src/Models/Device.php
@@ -6,31 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Device extends Model
 {
-
     /**
      * @var array
      */
     protected $guarded = ['id', 'created_at', 'updated_at'];
 
     /**
-     * Devices table
+     * Devices table.
+     *
      * @var string
      */
     protected $table = 'devices';
 
     /**
-     * Device token
+     * Device token.
+     *
      * @var array
      */
-    protected $fillable = ['user_id','device_token'];
+    protected $fillable = ['user_id', 'device_token'];
 
     /**
      * Device belongs to specific user.
+     *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
     public function user()
     {
         return $this->belongsTo(config('deeplink.user_class'));
     }
-
 }
--- src/config/deeplink.php
+++ src/config/deeplink.php
@@ -11,9 +11,8 @@ return [
     |  Application from which you can enable push notifications for
     |  you app.
     */
-    'one_signal_app_id' => env('ONE_SIGNAL_APP_ID', null),
+    'one_signal_app_id'  => env('ONE_SIGNAL_APP_ID', null),
     'one_signal_api_key' => env('ONE_SIGNAL_API_KEY', null),
-
 
     /*
     |--------------------------------------------------------------------------
@@ -24,11 +23,9 @@ return [
     |
     */
 
-    "cloudinary_cloud_name" => env('CLOUDINARY_CLOUD_NAME', null),
-    "cloudinary_api_key" => env('CLOUDINARY_API_KEY', null),
-    "cloudinary_api_secret" => env('CLOUDINARY_API_SECRET', null),
-
-
+    'cloudinary_cloud_name' => env('CLOUDINARY_CLOUD_NAME', null),
+    'cloudinary_api_key'    => env('CLOUDINARY_API_KEY', null),
+    'cloudinary_api_secret' => env('CLOUDINARY_API_SECRET', null),
 
     /*
     |--------------------------------------------------------------------------
@@ -37,8 +34,6 @@ return [
     |
     */
 
-    'user_class'=>'App\User'
-
-
+    'user_class'=> 'App\User',
 
 ];
